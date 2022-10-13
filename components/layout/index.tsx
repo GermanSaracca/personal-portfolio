2b2ee6085d388f3b3ffe139a8bd489a2f1883084
Germán Saracca <gersaracca@gmail.com>
2022-10-13 15:21:23 -0300

    fix margin auto mobile

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -12,7 +12,7 @@ const Layout = ({ children }: Props) => {
         <div className="flex flex-col bg-gray-200 dark:bg-gray-800 min-h-screen">
             <Header />
 
-            <main className="text-gray-800 dark:text-gray-200 overflow-x-hidden max-w-screen-2xl mx-auto">
+            <main className="text-gray-800 dark:text-gray-200 overflow-x-hidden max-w-screen-2xl xl:mx-auto">
                 {children}
             </main>
             <GoUpButton />
