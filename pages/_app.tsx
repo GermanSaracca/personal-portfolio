88016cf64209af89d82c2ac9d701528aef07ce99
Germán Saracca <gersaracca@gmail.com>
2022-10-13 20:00:54 -0300

    Add Google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import Layout from '../components/layout'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 import { ColorThemeProvider } from '../context/ThemeProvider'
 import { appWithTranslation } from 'next-i18next'
 import { useRouter } from 'next/router'
@@ -13,6 +14,23 @@ function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ColorThemeProvider>
             <Layout>
+                <Script
+                    strategy="afterInteractive"
+                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+                />
+                <Script
+                    id="gtag-init"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+                    `,
+                    }}
+                />
+
                 <Head>
                     <title>Germán Saracca Portfolio</title>
                 </Head>
